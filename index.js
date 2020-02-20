f26cca18a9977eccd295de513a8200c660dc0bb0
Alberto Ammar <albertoammarw@gmail.com>
2020-02-19 21:35:55 -0300

    fix: indent in tsx, jsx files

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ module.exports = {
     parser: '@typescript-eslint/parser', // Specifies the ESLint parser
     extends: [
         'airbnb-typescript',
+        'plugin:react/recommended',
         'plugin:jest/recommended',
         'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
         'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
         'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+        'prettier/react',
     ],
     plugins: ['jest'],
     parserOptions: {
@@ -18,12 +20,23 @@ module.exports = {
         'comma-dangle': 'off',
         'class-methods-use-this': 'off',
         '@typescript-eslint/explicit-member-accessibility': 'off',
-        '@typescript-eslint/interface-name-prefix': 'off',
         '@typescript-eslint/no-unused-vars': 'off',
         '@typescript-eslint/camelcase': ['error', { properties: 'always' }],
         '@typescript-eslint/explicit-member-accessibility': 'off',
         '@typescript-eslint/interface-name-prefix': ['error', { prefixWithI: 'always' }],
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/explicit-function-return-type': [
+            'error',
+            {
+                // if true, only functions which are part of a declaration will be checked
+                allowExpressions: true,
+            },
+        ],
+        // React
+        'react/prefer-stateless-function': 'off',
+        'react/jsx-indent': 'off',
+        'react/jsx-props-no-spreading': 'off', // habilitar <LoginScreen {...loginScreen} />
+        'react/state-in-constructor': ['warn', 'always'],
     },
 };
